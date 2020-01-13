4595cc9159043ea9de38446172a21062b8b97204
Kennedykid1995 <34872885+Kennedykid1995@users.noreply.github.com>
2020-01-12 19:27:22 -0600

    trying to figure out error

--- api-work/src/App.js
+++ api-work/src/App.js
@@ -5,27 +5,22 @@ import './App.css';
 // http://www.omdbapi.com/?i=tt3896198&apikey=70e206aa
 // http://img.omdbapi.com/?i=tt3896198&apikey=70e206aa
 
-function App() {
+const App = () => {
 
-  const [pokemon, setPokemon] = useState({pokedex: []})
+  const [pokemon, setPokemon] = useState([])
 
   useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios(
-        'https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20'
-      );
-      setPokemon(result.data);
-      console.log(result); 
-    };
-    fetchData();
+    axios.get('https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20')
+    .then(res => {
+      setPokemon(res.data);
+      console.log(res.data)
+    });
   }, [])
-
   return (
     <div className="App">
-      {pokemon.pokedex.map(poke => (
-        <li>{poke.name}</li>
-      )
-      )}
+      {pokemon.map(poke => (
+        <h1>{poke.name}</h1>
+      ))}
     </div>
   );
 }
